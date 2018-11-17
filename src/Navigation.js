c5ca86842f379dd6aaf4c4fbcda318991f4657c3
Johnny Reina <johnny.r.reina@gmail.com>
2018-11-17 01:40:47 -0600

    Adds link to admin

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Collapse,
+  NavbarToggler,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-const Navigation = ({ children }) => {
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">node media server</NavbarBrand>
-        <Nav>
-          <NavItem>{children}</NavItem>
-        </Nav>
-      </Navbar>
-    </div>
-  );
-};
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+  toggle = () => {
+    this.setState(({ open }) => ({ open: !open }));
+  };
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">node media server</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.open} className="ml-auto" navbar>
+            <Nav>
+              <NavItem>
+                <NavLink href="/admin">Admin</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default Navigation;
