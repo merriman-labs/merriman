b1fb9c3a16b615d16263bf07ea67171dc311065e
Johnny Reina <johnny.r.reina@gmail.com>
2018-11-17 01:13:01 -0600

    Adds route for media selection component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from './Navigation';
 import VideoLibrariesPage from './VideoLibrariesPage';
 import { Container, Row, Col } from 'reactstrap';
 import Home from './Home';
+import SelectMedia from './admin/SelectMedia';
 
 import AdminPanel from './admin/AdminPanel';
 class App extends Component {
@@ -33,7 +34,12 @@ class App extends Component {
             <Route exact path="/" component={Home} />
           </Col>
         </Row>
-        <Route path="/admin" component={AdminPanel} />
+        <Route exact path="/admin" component={AdminPanel} />
+        <Route
+          exact
+          path="/admin/select-media/:library"
+          component={SelectMedia}
+        />
       </Container>
     );
   }
