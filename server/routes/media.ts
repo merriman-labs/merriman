f4c50fbc43eb409cf4b903640df03a330ade4c6e
Johnny Reina <johnny.r.reina@gmail.com>
2018-12-25 01:54:28 -0600

    Cleans up exports and makes thumbnails not a thing rn

--- server/routes/media.ts
+++ server/routes/media.ts
@@ -1,4 +1,4 @@
-import { Router, Response, Request } from 'express';
+import { Router } from 'express';
 import LibraryRepo from '../data/LibraryRepo';
 import ServerConfigRepo from '../data/ServerConfigRepo';
 import ThumbProvider from '../thumb-provider';
