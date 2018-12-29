83eee80f4dde91ad73a5c0d55c8a128def139c70
Johnny Reina <johnny.r.reina@gmail.com>
2018-12-29 15:58:18 -0600

    Adds help screen and editconfig to cli commands

--- server/cli/index.ts
+++ server/cli/index.ts
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
+
 import app from '../app';
 import initDir from './init-dir';
+import editConfig from './editconfig';
+import printOptions from './print-options';
 import { argv } from 'process';
 import { eject } from './util/eject';
 
 (async function main() {
-  console.log(argv);
   switch (argv[2]) {
     case 'initdir':
       await initDir();
+      eject();
       break;
     case 'run':
       app();
       break;
+    case 'editconfig':
+      editConfig();
+      eject();
+      break;
     default:
+      printOptions();
       eject();
       break;
   }
