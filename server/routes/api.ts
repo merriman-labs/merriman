bf59fe85e0f7871434dea39716fd8d67fde0b0ad
Johnny Reina <johnny.r.reina@gmail.com>
2018-12-30 02:54:34 -0600

    WIP

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import LibraryRouter from './library';
 import AdminRouter from './admin';
 import MediaRouter from './media';
+import SearchRouter from './search';
 
 const apiRouter = Router();
 
-apiRouter.use('/library', LibraryRouter);
 apiRouter.use('/admin', AdminRouter);
+apiRouter.use('/library', LibraryRouter);
+apiRouter.use('/search', SearchRouter);
 apiRouter.use('/media', MediaRouter);
 
 export default apiRouter;
---
+++ server/routes/search.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import MediaItemRepo from '../data/MediaRepo';
+
+const searchRouter = Router();
+const mediaRepo = new MediaItemRepo();
+
+searchRouter.get('/:term', async function(req, res) {
+  const term = req.params.term;
+  const results = mediaRepo.where(item => JSON.stringify(item).includes(term));
+
+  if (!results) return res.status(500).json({ message: 'Library not found!' });
+  res.json({ results });
+});
+
+export default searchRouter;
