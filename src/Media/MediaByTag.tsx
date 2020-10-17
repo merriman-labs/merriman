700daaac369092b53600dcac76fe90967bc34ec7
Johnny Reina <johnny.r.reina@gmail.com>
2020-10-16 23:48:18 -0500

    Layout changes in tag page

--- src/Media/MediaByTag.tsx
+++ src/Media/MediaByTag.tsx
@@ -2,6 +2,8 @@ import * as R from 'ramda';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardImg, CardImgOverlay, CardText, Col } from 'reactstrap';
+import Container from 'reactstrap/lib/Container';
+import Row from 'reactstrap/lib/Row';
 import { MediaItem } from '../../server/models';
 import MediaManager from '../managers/MediaManager';
 
@@ -31,23 +33,25 @@ export const MediaByTag = (props: MediaByTagProps) => {
     [props.match.params.tag]
   );
   return (
-    <>
-      {R.splitEvery(4, media).map(group => {
-        return group.map((item, i) => {
-          return (
-            <Col sm="6" lg="3" key={i} className="video-cell">
-              <Card>
-                <Link to={`/media/${item._id.toString()}`}>
-                  <CardImg src={`/${item.filename}.png`} />
-                  <CardImgOverlay className="thumbnail-link">
-                    <CardText>{item.name}</CardText>
-                  </CardImgOverlay>
-                </Link>
-              </Card>
-            </Col>
-          );
-        });
-      })}
-    </>
+    <Container fluid>
+      <Row>
+        {R.splitEvery(4, media).map(group =>
+          group.map((item, i) => {
+            return (
+              <Col sm="6" lg="3" xl="2" key={i} className="video-cell">
+                <Card>
+                  <Link to={`/media/${item._id.toString()}`}>
+                    <CardImg src={`/${item.filename}.png`} />
+                    <CardImgOverlay className="thumbnail-link">
+                      <CardText>{item.name}</CardText>
+                    </CardImgOverlay>
+                  </Link>
+                </Card>
+              </Col>
+            );
+          })
+        )}
+      </Row>
+    </Container>
   );
 };
