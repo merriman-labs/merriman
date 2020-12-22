9bfda3f0d5fca75c62fb101601e724307632d736
Johnny Reina <johnny.r.reina@gmail.com>
2020-12-21 21:28:54 -0600

    Replace bcrypt dep

--- server/Engines/AuthEngine.ts
+++ server/Engines/AuthEngine.ts
@@ -1,9 +1,9 @@
 import { injectable } from 'inversify';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @injectable()
 export class AuthEngine {
   doesPasswordMatch(password: string, hash: string) {
-    return bcrypt.compare(password, hash);
+    return bcrypt.compareSync(password, hash);
   }
 }
--- server/models/User/User.ts
+++ server/models/User/User.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { ObjectId } from 'mongodb';
 import { UserRole } from '../../Constant/UserRole';
 import { UserCreatePayload } from '../../Managers/UserManager';
