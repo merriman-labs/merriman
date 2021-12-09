b7a35cf191f7f02dcd7cab435261548065398904
Johnny Reina <johnny.r.reina@gmail.com>
2021-12-09 00:50:41 -0600

    Add date to migration dashboard

--- src/admin/MigrationDashboard.tsx
+++ src/admin/MigrationDashboard.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import { FaFile } from 'react-icons/fa';
 import { MediaItem } from '../../server/models';
@@ -28,12 +29,15 @@ export const MigrationDashboard = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-8">
           <ul className="list-group">
             {items.map((item) => (
               <li className="list-group-item d-flex justify-content-between">
                 <span className="d-block filelist-item">
-                  <FaFile /> {item.name}
+                  <FaFile /> {item.name}{' '}
+                  <small className="text-muted">
+                    {moment(item.createdAt).format('MM/DD/YYYY HH:mm')}
+                  </small>
                 </span>
                 <button
                   className="btn btn-primary"
